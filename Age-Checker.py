084cdf8812bb55aec71155b7b5f9ace75f0e9682
Nishchaya-Code <nirjanduwal@gmail.com>
2025-03-16 07:39:48 +0545

    Add files via upload

---
+++ Age-Checker.py
@@ -0,0 +1,7 @@
+Age=int(input('enter your age'))
+if Age<10:
+    print ('you are not allowed')
+elif Age>=10 and Age<=120:
+    print('you are allowed')
+else:
+    print ('you are not allowed')
---
+++ Square-roots.py
@@ -0,0 +1,2 @@
+x=int(input('input any square number'))
+print(x**0.5,'is the squre root')
