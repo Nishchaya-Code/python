4e3562b48d759fb02720fa3d20f25c0136cc8267
Nishchaya-Code <nirjanduwal@gmail.com>
2025-03-16 08:40:36 +0545

    Add files via upload

---
+++ Reverse-strings.py
@@ -0,0 +1,6 @@
+x=input('input any text')
+y=''
+for i in x:
+    y=i+y
+print('this is the original string',x)
+print('this is the reverse string',y)
---
+++ revese-order.py
@@ -0,0 +1,2 @@
+for i in range(66,1,-1):
+    print(i)
