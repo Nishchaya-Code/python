4eed6cdcb56e0bdecf1a38a2efc19762bc2a4d3b
Nishchaya-Code <nirjanduwal@gmail.com>
2025-04-04 18:15:38 +0545

    Add files via upload

---
+++ Turtle-Square.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.Screen().bgcolor('red')
+turtle.Screen().setup(400,400)
+polygon=turtle.Turtle()
+sides=4
+angle=360/sides
+for i in range(sides):
+    polygon.forward(69)
+    polygon.right(angle)
+turtle.done()
